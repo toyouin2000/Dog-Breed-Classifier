74ffd56f498ff8703343fc7a8f301b78121e6d36
Dishank <82960196+toyouin2000@users.noreply.github.com>
2024-04-08 15:50:38 +0530

    Add files via upload

---
+++ extract_bottleneck_features.py
@@ -0,0 +1,11 @@
+def extract_VGG16(tensor):
+	from keras.applications.vgg16 import VGG16, preprocess_input
+	return VGG16(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+def extract_VGG19(tensor):
+	from keras.applications.vgg19 import VGG19, preprocess_input
+	return VGG19(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
+
+def extract_Resnet50(tensor):
+	from keras.applications.resnet50 import ResNet50, preprocess_input
+	return ResNet50(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
